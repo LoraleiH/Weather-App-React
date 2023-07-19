6ba8432d81dfef3a32d63abeac63a5d76c25f7c1
LoraleiH <106624828+LoraleiH@users.noreply.github.com>
2023-07-19 14:59:31 +0100

    Forecast Prep
    added in HTML for forecast from previous build
    
    changed open source link to correct directory

--- src/Weather.js
+++ src/Weather.js
@@ -82,13 +82,28 @@ setCity(event.target.value);
 
             <hr />
 
-            <div className="forecast-week"></div>
+            <div className="forecast-week">
+              <div class="col" id="forecast-col">
+                <div class="forecast-day">Monday</div>
+                <img
+                  class="forecast-icon"
+                  src="http://openweathermap.org/img/wn/01n@2x.png"
+                  alt="description"
+                />
+
+                <div>
+                  <span class="high">19°</span> <span class="low">9°</span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <br />
 
         <div class="credit">
-          <a href="https://github.com/LoraleiH/Weather-App">Open Source Code</a>{" "}
+          <a href="https://github.com/LoraleiH/Weather-App-React">
+            Open Source Code
+          </a>{" "}
           by Loralei Heathcote
         </div>
       </div>
